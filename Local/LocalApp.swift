0efa0936573a4106981259107e7fa0a0e0f016a9
divasatanica <jayhebes@vip.qq.com>
2024-09-21 20:45:36 +0800

    Initial Commit

---
+++ Local/LocalApp.swift
@@ -0,0 +1,32 @@
+//
+//  LocalApp.swift
+//  Local
+//
+//  Created by mac on 2024/9/21.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LocalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ LocalTests/LocalTests.swift
@@ -0,0 +1,16 @@
+//
+//  LocalTests.swift
+//  LocalTests
+//
+//  Created by mac on 2024/9/21.
+//
+
+import Testing
+
+struct LocalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
